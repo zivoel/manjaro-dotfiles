2e8481b7cecc69de6cfac91606369917f075d82f
J. Vitor <62527794+zivoel@users.noreply.github.com>
2021-02-15 02:04:56 -0300

    Delete dotfiles/config directory

--- dotfiles/config/my_scripts/my_monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VGA-1 --mode 1360x768 --pos 0x312 --rotate normal --output DVI-D-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 1360x0 --rotate normal
--- dotfiles/config/my_scripts/rofi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rofi -modi "window,drun" -show drun -sidebar-mode -show-icons   
